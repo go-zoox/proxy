0ead70367f9c7d537420c2b9960571e12053d4a3
whatwewant <tobewhatwewant@outlook.com>
2022-06-14 14:29:08 +0800

    chore(example): add chi

---
+++ example/chi/go.mod
@@ -0,0 +1,13 @@
+module example
+
+go 1.18
+
+require (
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-zoox/proxy v1.0.1
+)
+
+require (
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ example/chi/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-zoox/proxy"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+
+	p := proxy.NewSingleTarget("https://httpbin.zcorky.com", &proxy.SingleTargetConfig{
+		Rewrites: map[string]string{
+			"^/api/(.*)": "/$1",
+		},
+	})
+
+	r.Get("/api/*", func(w http.ResponseWriter, r *http.Request) {
+		p.ServeHTTP(w, r)
+	})
+
+	http.ListenAndServe(":8080", r)
+}
