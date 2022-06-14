a4667e4fc6dd1acffe0aad1a155af3a738a32326
whatwewant <tobewhatwewant@outlook.com>
2022-06-14 14:24:33 +0800

    chore(example): add gin

---
+++ example/gin/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-zoox/proxy"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"hello": "world",
+		})
+	})
+
+	p := proxy.NewSingleTarget("https://httpbin.zcorky.com", &proxy.SingleTargetConfig{
+		Rewrites: map[string]string{
+			"^/api/(.*)": "/$1",
+		},
+	})
+
+	// r.GET("/api/*path", func(ctx *gin.Context) {
+	// 	// ctx.Request.URL.Path = re.ReplaceAllString(ctx.Request.URL.Path, "/$1")
+	// 	p.ServeHTTP(ctx.Writer, ctx.Request)
+	// })
+
+	r.GET("/api/*path", gin.WrapH(p))
+
+	r.Run()
+}
