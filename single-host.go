2b5c9441600ccb6413e2bb9ba49e53193c1de577
Zero <tobewhatwewant@gmail.com>
2023-09-08 10:40:46 +0800

    chore: refactor single-host with simple target only is allow

--- single-host.go
+++ single-host.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"regexp"
 
 	"github.com/go-zoox/headers"
 	"github.com/go-zoox/proxy/utils/rewriter"
@@ -27,6 +26,65 @@ type SingleHostConfig struct {
 }
 
 // NewSingleHost creates a new Single Host Proxy.
+// target is the URL of the host you wish to proxy to.
+// cfg is the configuration for the SingleHost.
+//   - Rewrites is the rewriters for the SingleHost.
+//   - Scheme overrides the scheme of target.
+//   - Query is the query of the SingleHost.
+//   - RequestHeaders is the request headers of the SingleHost.
+//   - ResponseHeaders is the response headers of the SingleHost.
+//   - OnRequest is the hook that is called before the request is sent.
+//   - OnResponse is the hook that is called after the response is received.
+//   - IsAnonymouse is a flag to indicate whether the proxy is anonymouse.
+//     which means the proxy will not add headers:
+//     X-Forwarded-For
+//     X-Forwarded-Proto
+//     X-Forwarded-Host
+//     X-Forwarded-Port
+//     Default is false.
+//   - ChangeOrigin is a flag to indicate whether the proxy will change the origin.
+//     which means the proxy will change the origin to target.
+//     Default is false.
+//   - OnError is the hook that is called when an error occurs.
+//
+// Example:
+//
+//	// All requests will be redirected to https://httpbin.org
+//	NewSingleHost("https://httpbin.org")
+//
+//	// All requests will be redirected to https://httpbin.org/ip
+//	NewSingleHost("https://httpbin.org/ip")
+//
+//	// All requests will be redirected to https://httpbin.org with Rewrites
+//	 NewSingleHost("https://httpbin.org", &SingleHostConfig{
+//	 	Rewrites: rewriter.Rewriters{
+//	 		{ From: "/api/get", To: "/get" },
+//		  { From: "/api/post", To: "/post" },
+//	 	  { From: "/api/v2/(.*)", To: "/$1" },
+//	 	},
+//	 	Query: url.Values{
+//	 		"foo": []string{"bar"},
+//	 	},
+//	 	RequestHeaders: http.Header{
+//	 		headers.Host: []string{"httpbin.org"},
+//	 	},
+//	 	ResponseHeaders: http.Header{
+//	 		headers.Server: []string{"go-zoox_proxy"},
+//	 	},
+//	 	OnRequest: func(req *http.Request) error {
+//	 		// do something
+//	 		return nil
+//	 	},
+//	 	OnResponse: func(res *http.Response) error {
+//	 		// do something
+//	 		return nil
+//	 	},
+//	 	IsAnonymouse: true,
+//	 	ChangeOrigin: true,
+//	 	OnError: func(err error, rw http.ResponseWriter, req *http.Request) {
+//	 		// do something
+//	 	},
+//	 })
 func NewSingleHost(target string, cfg ...*SingleHostConfig) *Proxy {
 	var onRequest func(req *http.Request) error
 	var onResponse func(res *http.Response) error
@@ -36,20 +94,11 @@ func NewSingleHost(target string, cfg ...*SingleHostConfig) *Proxy {
 	var isAnonymouse bool
 	var changeOrigin bool
 	var onError func(err error, rw http.ResponseWriter, req *http.Request)
+	var rewriters rewriter.Rewriters
 
-	host := target
-	scheme := "http"
-	rewriters := rewriter.Rewriters{}
-
-	if re, err := regexp.Compile(`^(.+)://([^/]+)`); err != nil {
-		panic(fmt.Errorf("regexp compile error: %s", err))
-	} else {
-		if matched := re.FindStringSubmatch(target); matched != nil {
-			scheme = matched[1]
-			host = matched[2]
-		} else {
-			panic(fmt.Errorf("invalid proxy target: %s", target))
-		}
+	targetX, err := url.Parse(target)
+	if err != nil {
+		panic(fmt.Errorf("invalid proxy target: %s", err))
 	}
 
 	if len(cfg) > 0 {
@@ -57,7 +106,7 @@ func NewSingleHost(target string, cfg ...*SingleHostConfig) *Proxy {
 			rewriters = cfg[0].Rewrites
 		}
 		if cfg[0].Scheme != "" {
-			scheme = cfg[0].Scheme
+			targetX.Scheme = cfg[0].Scheme
 		}
 		if cfg[0].Query != nil {
 			query = cfg[0].Query
@@ -102,12 +151,19 @@ func NewSingleHost(target string, cfg ...*SingleHostConfig) *Proxy {
 		requestHeaders.Set(headers.UserAgent, fmt.Sprintf("go-zoox_proxy/%s", Version))
 	}
 
+	isRewriteExist := len(rewriters) != 0
+
 	return New(&Config{
 		IsAnonymouse: isAnonymouse,
 		OnRequest: func(outReq, inReq *http.Request) error {
-			outReq.URL.Scheme = scheme
-			outReq.URL.Host = host
-			outReq.URL.Path = rewriters.Rewrite(outReq.URL.Path)
+			outReq.URL.Scheme = targetX.Scheme
+			outReq.URL.Host = targetX.Host
+
+			if isRewriteExist {
+				outReq.URL.Path = rewriters.Rewrite(outReq.URL.Path)
+			} else {
+				outReq.URL.Path = targetX.Path
+			}
 
 			if query != nil {
 				originQuery := outReq.URL.Query()
